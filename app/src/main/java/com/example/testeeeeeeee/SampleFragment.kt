41c9e8ef221b51e4a8976520c9a77761e41ff9c9
marcelodolacio <marcelodolacio@gmail.com>
2020-11-23 17:05:50 -0300

    Binding

--- app/src/main/java/com/example/testeeeeeeee/SampleFragment.kt
+++ app/src/main/java/com/example/testeeeeeeee/SampleFragment.kt
@@ -1,50 +1,37 @@
 package com.example.testeeeeeeee
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+
 
 import androidx.navigation.fragment.findNavController
+import com.example.testeeeeeeee.databinding.FragmentSampleBinding
 
 
 class SampleFragment : Fragment() {
 
+    private lateinit var binding: FragmentSampleBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
-        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_sample, container, false)
 
-        val view: View = inflater.inflate(R.layout.fragment_sample, container, false)
-        (activity as MainActivity?)!!.fragmentMethod()
-
-
-
-
-//        val btn = view.findViewById<View>(R.id.btnNha) as Button
-//
-//        btn.setOnClickListener{
-//            findNavController().navigate(R.id.action_sampleFragment_to_secundaryFragment)
-//        }
-
-        return view
-
+        binding = FragmentSampleBinding.inflate(inflater, container, false)
+        binding.fragment = this
+       // binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
-    fun register() {
-        println("other message")
-        println("other message")
-        println("other message")
-        println("other message")
-        println("other message")
-        println("other message")
-        println("other message")
-
-
+     fun register() {
+        Log.d("nha","BORAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
         findNavController().navigate(R.id.action_sampleFragment_to_secundaryFragment)
     }
 
